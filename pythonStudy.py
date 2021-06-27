0661041ea4ec6a5c3bdbcbe4dbad39af0b97e054
sugitecmake <m.akeda@sugitec.co.jp>
2021-06-27 11:10:19 +0900

    firstcommit

---
+++ pythonStudy.py
@@ -0,0 +1,29 @@
+# # rows = ["〇×〇", "×〇〇","〇××"]
+# # for i in range(3):
+# #     print(rows[i])
+
+# a = [0,0,0,0,0]
+
+# b = []
+# for i in range(5):
+#     b.append(i) 
+    
+# for i in a:
+#     print(i)
+# for j in b:
+#     print(j*2)
+# print(b)
+
+data = [i for i in range(5)]
+print(data)
+
+a = [[0]*3]*3
+a[1][1]= 1
+print(a)
+
+
+
+a = [[0]*3 for i in range(3)]
+a[1][1] = 1
+print(a)
+

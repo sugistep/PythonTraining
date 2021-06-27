1d94e28b42ee3072961161f24fc5bad47bb4207a
sugitecmake <m.akeda@sugitec.co.jp>
2021-06-27 15:55:02 +0900

    rows

--- pythonStudy.py
+++ pythonStudy.py
@@ -14,24 +14,30 @@
 #     print(j*2)
 # print(b)
 
-data = [i for i in range(5)]
-print(data)
+# data = [i for i in range(5)]
+# print(data)
 
-a = [[0]*3]*3
-a[1][1]= 1
-print(a)
-
-
-
-a = [[0]*3 for i in range(3)]
-a[1][1] = 1
-print(a)
+# a = [[0]*3]*3
+# a[1][1]= 1
+# print(a)
 
 
 
+# a = [[0]*3 for i in range(3)]
+# a[1][1] = 1
+# print(a)
 
 
+def print_board(rows):
+    for i in range(3):
+        for j in range (3):
+            print(rows[i][j], end='')
+        print()
+    print()
 
+def init_board():
+    rows = [['A'] * 3 for i in range(3)]
+    return rows
 
-
-
+rows = init_board()
+print_board(rows)
